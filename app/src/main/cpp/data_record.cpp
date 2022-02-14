dd51487bdac440a7a84e4407c08fb93a7ea5d3a5
ZhaoqunZhong <zhongzhaoqun1991@outlook.com>
2022-02-14 16:14:43 +0800

    get custom default values for data recording : Kotlin part

--- app/src/main/cpp/data_record.cpp
+++ app/src/main/cpp/data_record.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <jni.h>
+#include <vector>
+#include <string>
 
 extern "C"
 JNIEXPORT void JNICALL
@@ -15,14 +17,24 @@ Java_com_zhaoqun_slam_1app_ui_data_1record_DataRecordFragment_stopDumpJNI(JNIEnv
     // TODO: implement stopDumpJNI()
 }
 extern "C"
-JNIEXPORT jobject JNICALL
+JNIEXPORT jobjectArray JNICALL
 Java_com_zhaoqun_slam_1app_ui_data_1record_DataRecordFragment_getImuFreqs(JNIEnv *env,
                                                                           jobject thiz) {
-    // TODO: implement getImuFreqs()
+    std::vector<std::string> imu_freqs_str{"400", "800"};
+    jobjectArray imu_freqs = env->NewObjectArray(imu_freqs_str.size(), env->FindClass(
+            "java/lang/String"),env->NewStringUTF(""));
+    for (int i = 0; i < imu_freqs_str.size(); i++)
+        env->SetObjectArrayElement(imu_freqs, i, env->NewStringUTF(imu_freqs_str[i].c_str()));
+    return imu_freqs;
 }
 extern "C"
-JNIEXPORT jobject JNICALL
+JNIEXPORT jobjectArray JNICALL
 Java_com_zhaoqun_slam_1app_ui_data_1record_DataRecordFragment_getBackCamIDs(JNIEnv *env,
                                                                             jobject thiz) {
-    // TODO: implement getBackCamIDs()
+    std::vector<std::string> cams_str{"0", "1", "2"};
+    jobjectArray cams = env->NewObjectArray(cams_str.size(), env->FindClass(
+            "java/lang/String"),env->NewStringUTF(""));
+    for (int i = 0; i < cams_str.size(); i++)
+        env->SetObjectArrayElement(cams, i, env->NewStringUTF(cams_str[i].c_str()));
+    return cams;
 }
--- app/src/main/java/com/zhaoqun/slam_app/ui/data_record/DataRecordFragment.kt
+++ app/src/main/java/com/zhaoqun/slam_app/ui/data_record/DataRecordFragment.kt
@@ -1,5 +1,7 @@
 package com.zhaoqun.slam_app.ui.data_record
 
+import android.R
+import android.R.attr
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +14,18 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.zhaoqun.slam_app.databinding.FragmentDataBinding
 import com.zhaoqun.slam_app.ui.image_processing.GalleryViewModel
+import android.R.attr.data
+import android.content.Context
+import android.util.Log
+
+import android.widget.ArrayAdapter
+import com.zhaoqun.slam_app.file_server.FileSynchronizer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.File
+
 
 class DataRecordFragment : Fragment() {
     private val dataRecordViewModel: DataRecordViewModel by activityViewModels()
@@ -20,6 +34,7 @@ class DataRecordFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val debug_tag = "DataRecordFragment"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -67,10 +82,46 @@ class DataRecordFragment : Fragment() {
         dataRecordViewModel._imu_format.observe(viewLifecycleOwner, {binding.imuFile.setSelection(it)})
         dataRecordViewModel._imu_order.observe(viewLifecycleOwner, {binding.imuOrder.setSelection(it)})
         dataRecordViewModel._pack_rosbag.observe(viewLifecycleOwner, {binding.packRosbag.isChecked = it})
+
+        var cam_ids = arrayListOf<String>("-1", "-2")
+        var imu_freqs = arrayListOf<String>("-1", "-2")
+        @Serializable
+        data class ViSensorInfo(
+            val cam_ids: ArrayList<String>,
+            val imu_freqs: ArrayList<String>
+        )
+        val json = Json {
+            prettyPrint = true
+            prettyPrintIndent = "  "
+            coerceInputValues = true
+        }
+        val app_dir = activity?.filesDir.toString()
+        val sensor_info_file = File("$app_dir/vi_sensor_info.json")
+        if (sensor_info_file.exists()) {
+            Log.i(debug_tag, "Found vi sensor info file, read from file.")
+            val sensor_info_string = sensor_info_file.readText()
+            val sensor_info_data = json.decodeFromString<ViSensorInfo>(sensor_info_string)
+            cam_ids = sensor_info_data.cam_ids
+            imu_freqs = sensor_info_data.imu_freqs
+        } else {
+            cam_ids = getBackCamIDs().toMutableList() as ArrayList<String>
+            imu_freqs = getImuFreqs().toMutableList() as ArrayList<String>
+            var sensor_info_data = ViSensorInfo(cam_ids, imu_freqs)
+            val sensor_info_string = json.encodeToString(sensor_info_data)
+            sensor_info_file.writeText(sensor_info_string)
+        }
+
+        val aa = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, cam_ids)
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.camId.setAdapter(aa)
+
+        val aa1 = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, imu_freqs)
+        aa1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.imuFps.setAdapter(aa1)
     }
 
-    external fun getBackCamIDs() : ArrayList<Int>
-    external fun getImuFreqs() : ArrayList<Int>
+    external fun getBackCamIDs() : Array<String>
+    external fun getImuFreqs() : Array<String>
     external fun startDumpJNI()
     external fun stopDumpJNI()
 
